b94fea949ba4d98e7a957903679222d878e2062b
Subhaswini <cjsubbu@gmail.com>
2020-05-07 20:09:20 +0530

    testmessage

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4179ce9e-1aa9-4eeb-a372-32d53d1c64ef</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c9226d2-1851-409d-84a3-7aea97761693</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by CJ</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
